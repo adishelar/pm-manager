5a7f36d56ad674d7e56f6923a2b71464c6e74476
adishelar <adishelar399@gmail.com>
2025-11-19 21:21:18 +0530

    backend code

---
+++ PM-scheduler/src/main/java/com/PM_scheduler/Service/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.PM_scheduler.Service;
+import org.springframework.context.annotation.Bean;
+	import org.springframework.context.annotation.Configuration;
+	import org.springframework.web.servlet.config.annotation.CorsRegistry;
+	import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+	@Configuration
+	public class CorsConfig {
+
+	    @Bean
+	    WebMvcConfigurer corsConfigurer() {
+	        return new WebMvcConfigurer() {
+	            @Override
+	            public void addCorsMappings(CorsRegistry registry) {
+	                registry.addMapping("/**")
+	                        .allowedOrigins("https://adishelar.github.io")
+	                        .allowedMethods("*")
+	                        .allowedHeaders("*")
+	                        .allowCredentials(true);
+	            }
+	        };
+	    }
+	}
+
+
+
