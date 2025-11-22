7a82188bbba16f1223c79aa1037ccfe7726e20b4
Akash shelar <adishelar399@gmail.com>
2025-11-22 11:05:07 +0530

    Update SupervisorController.java

--- PMscheduler/src/main/java/com/PM_scheduler/Controller/SupervisorController.java
+++ PMscheduler/src/main/java/com/PM_scheduler/Controller/SupervisorController.java
@@ -4,15 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class SupervisorController {
+public class SupervisorController{
 
     @GetMapping("/Supervisor/login")
-    public String login() {
-        return "Supervisor-Login";  // your HTML file
+    public String login(){
+        return "Supervisor-Login";  // superviosr HTML file
     }
 
     @GetMapping("/Supervisor/home")
-    public String supervisorHome() {
+    public String supervisorHome(){
         return "ok";
     }
 }
